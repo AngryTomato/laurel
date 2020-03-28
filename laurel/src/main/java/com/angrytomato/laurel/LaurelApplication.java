a60104bb66cad49df255636b6f8b3b8754f65ec7
AngryTomato <cnsummerchang@outlook.com>
2020-03-28 21:26:52 +0800

    开始新项目

---
+++ laurel/src/main/java/com/angrytomato/laurel/LaurelApplication.java
@@ -0,0 +1,13 @@
+package com.angrytomato.laurel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaurelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaurelApplication.class, args);
+	}
+
+}
